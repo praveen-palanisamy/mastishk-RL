3d1d5f897bec3a54609be4104479431bd62b9725
adwaith <adwaith1990@gmail.com>
2019-02-28 23:43:47 -0700

    Created advection env class and defined the observations, rewards, etc. for the advection problem

--- gym-advection/gym_advection/envs/advection_env.py
+++ gym-advection/gym_advection/envs/advection_env.py
@@ -1,7 +1,5 @@
 """
-Classic cart-pole system implemented by Rich Sutton et al.
-Copied from http://incompleteideas.net/sutton/book/code/pole.c
-permalink: https://perma.cc/C9ZM-652R
+This problem is designed by Adwaith Gupta, 2019
 """
 
 import math
@@ -13,181 +11,95 @@ import numpy as np
 class AdvectionEnv(gym.Env):
     """
     Description:
-        A pole is attached by an un-actuated joint to a cart, which moves along a frictionless track. The pendulum starts upright, and the goal is to prevent it from falling over by increasing and reducing the cart's velocity.
+        Advection equation defines propogation of a disturbace at a certain propogation speed. The equation is given as:
+            
+            du/dt + a du/dx=0
+            
+            where,
+            u----> velocity
+            t----> time
+            a----> propagation (information) speed
+            x----> space
+            d represents a partial derivative, for example du/dt means partial derivative of velocity w.r.t time
+        
+        The propagation is defined as a Gaussian wave in this case:
+            U(x,t) = exp(-200*(x-xc-v*t).^2)
+            
+        There are many numerical schemes to solve the advection equation for the value of u. But all suffer with numerical errors. The task of the agent is to maintain the numerical solution (Lax-Wendroff) as close as possible (error<0.11) to the exact known solution for the longest time possible (normalized max time) by adjusting the time step (del_t) in the numerical scheme.
 
     Source:
-        This environment corresponds to the version of the cart-pole problem described by Barto, Sutton, and Anderson
+        This problem is designed by Adwaith Gupta, 2019
 
     Observation: 
-        Type: Box(4)
+        Type: Box(2)?
         Num	Observation                 Min         Max
-        0	Cart Position             -4.8            4.8
-        1	Cart Velocity             -Inf            Inf
-        2	Pole Angle                 -24°           24°
-        3	Pole Velocity At Tip      -Inf            Inf
+        0   Error                       0.0         0.11 
+        1   Current time step (del_t)   0.0         1.00 
         
     Actions:
         Type: Discrete(2)
         Num	Action
-        0	Push cart to the left
-        1	Push cart to the right
+        0	Do not change del_t
+        1	Reduce del_t by 1e-8
         
-        Note: The amount the velocity is reduced or increased is not fixed as it depends on the angle the pole is pointing. This is because the center of gravity of the pole increases the amount of energy needed to move the cart underneath it
-
     Reward:
-        Reward is 1 for every step taken, including the termination step
+        +1 if the error decreases (or stays the same) at any point
+        -1 if the error increases at any point
+        +100 if the normalized max time > 0.95 is attained
 
     Starting State:
-        All observations are assigned a uniform random value between ±0.05
+        All observations are assigned a uniform random value as follows:
+            Space step (del_x) E [800, 1200]
+            Propogation speed (a) E [0.8, 1.2]
 
     Episode Termination:
-        Pole Angle is more than ±12°
-        Cart Position is more than ±2.4 (center of the cart reaches the edge of the display)
+        Error > 0.11
         Episode length is greater than 200
-        Solved Requirements
-        Considered solved when the average reward is greater than or equal to 195.0 over 100 consecutive trials.
+        Considered solved when the normalized max time > 0.95
     """
     
-    metadata = {
-        'render.modes': ['human', 'rgb_array'],
-        'video.frames_per_second' : 50
-    }
-
+    metadata = {'render.modes': ['human']}
     def __init__(self):
-        self.gravity = 9.8
-        self.masscart = 1.0
-        self.masspole = 0.1
-        self.total_mass = (self.masspole + self.masscart)
-        self.length = 0.5 # actually half the pole's length
-        self.polemass_length = (self.masspole * self.length)
-        self.force_mag = 10.0
-        self.tau = 0.02  # seconds between state updates
-        self.kinematics_integrator = 'euler'
-
-        # Angle at which to fail the episode
-        self.theta_threshold_radians = 12 * 2 * math.pi / 360
-        self.x_threshold = 2.4
-
-        # Angle limit set to 2 * theta_threshold_radians so failing observation is still within bounds
-        high = np.array([
-            self.x_threshold * 2,
-            np.finfo(np.float32).max,
-            self.theta_threshold_radians * 2,
-            np.finfo(np.float32).max])
-
-        self.action_space = spaces.Discrete(2)
-        self.observation_space = spaces.Box(-high, high, dtype=np.float32)
-
-        self.seed()
-        self.viewer = None
-        self.state = None
-
-        self.steps_beyond_done = None
-
-    def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
+        self.__version__ = "0.0.1"
+        # Modify the observation space, low, high and shape values according to your custom environment's needs
+        self.observation_space = gym.spaces.Box(low=0.0, high=1.0, shape=(2,)) #high=?
+        # Modify the action space, and dimension according to your custom environment's needs
+        self.action_space = gym.spaces.Discrete(2)
 
     def step(self, action):
-        assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
-        state = self.state
-        x, x_dot, theta, theta_dot = state
-        force = self.force_mag if action==1 else -self.force_mag
-        costheta = math.cos(theta)
-        sintheta = math.sin(theta)
-        temp = (force + self.polemass_length * theta_dot * theta_dot * sintheta) / self.total_mass
-        thetaacc = (self.gravity * sintheta - costheta* temp) / (self.length * (4.0/3.0 - self.masspole * costheta * costheta / self.total_mass))
-        xacc  = temp - self.polemass_length * thetaacc * costheta / self.total_mass
-        if self.kinematics_integrator == 'euler':
-            x  = x + self.tau * x_dot
-            x_dot = x_dot + self.tau * xacc
-            theta = theta + self.tau * theta_dot
-            theta_dot = theta_dot + self.tau * thetaacc
-        else: # semi-implicit euler
-            x_dot = x_dot + self.tau * xacc
-            x  = x + self.tau * x_dot
-            theta_dot = theta_dot + self.tau * thetaacc
-            theta = theta + self.tau * theta_dot
-        self.state = (x,x_dot,theta,theta_dot)
-        done =  x < -self.x_threshold \
-                or x > self.x_threshold \
-                or theta < -self.theta_threshold_radians \
-                or theta > self.theta_threshold_radians
-        done = bool(done)
-
-        if not done:
-            reward = 1.0
-        elif self.steps_beyond_done is None:
-            # Pole just fell!
-            self.steps_beyond_done = 0
-            reward = 1.0
-        else:
-            if self.steps_beyond_done == 0:
-                logger.warn("You are calling 'step()' even though this environment has already returned done = True. You should always call 'reset()' once you receive 'done = True' -- any further steps are undefined behavior.")
-            self.steps_beyond_done += 1
-            reward = 0.0
-
-        return np.array(self.state), reward, done, {}
+        """
+        Runs one time-step of the environment's dynamics. The reset() method is called at the end of every episode
+        :param action: The action to be executed in the environment
+        :return: (observation, reward, done, info)
+            observation (object):
+                Observation from the environment at the current time-step
+            reward (float):
+                Reward from the environment due to the previous action performed
+            done (bool):
+                a boolean, indicating whether the episode has ended
+            info (dict):
+                a dictionary containing additional information about the previous action
+        """
+        # Implement your step method here
+        # return (observation, reward, done, info)
 
     def reset(self):
-        self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(4,))
-        self.steps_beyond_done = None
-        return np.array(self.state)
+        """
+        Reset the environment state and returns an initial observation
 
-    def render(self, mode='human'):
-        screen_width = 600
-        screen_height = 400
+        Returns
+        -------
+        observation (object): The initial observation for the new episode after reset
+        :return:
+        """
 
-        world_width = self.x_threshold*2
-        scale = screen_width/world_width
-        carty = 100 # TOP OF CART
-        polewidth = 10.0
-        polelen = scale * (2 * self.length)
-        cartwidth = 50.0
-        cartheight = 30.0
+        # Implement your reset method here
+        # return observation
 
-        if self.viewer is None:
-            from gym.envs.classic_control import rendering
-            self.viewer = rendering.Viewer(screen_width, screen_height)
-            l,r,t,b = -cartwidth/2, cartwidth/2, cartheight/2, -cartheight/2
-            axleoffset =cartheight/4.0
-            cart = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
-            self.carttrans = rendering.Transform()
-            cart.add_attr(self.carttrans)
-            self.viewer.add_geom(cart)
-            l,r,t,b = -polewidth/2,polewidth/2,polelen-polewidth/2,-polewidth/2
-            pole = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
-            pole.set_color(.8,.6,.4)
-            self.poletrans = rendering.Transform(translation=(0, axleoffset))
-            pole.add_attr(self.poletrans)
-            pole.add_attr(self.carttrans)
-            self.viewer.add_geom(pole)
-            self.axle = rendering.make_circle(polewidth/2)
-            self.axle.add_attr(self.poletrans)
-            self.axle.add_attr(self.carttrans)
-            self.axle.set_color(.5,.5,.8)
-            self.viewer.add_geom(self.axle)
-            self.track = rendering.Line((0,carty), (screen_width,carty))
-            self.track.set_color(0,0,0)
-            self.viewer.add_geom(self.track)
+    def render(self, mode='human', close=False):
+        """
 
-            self._pole_geom = pole
-
-        if self.state is None: return None
-
-        # Edit the pole polygon vertex
-        pole = self._pole_geom
-        l,r,t,b = -polewidth/2,polewidth/2,polelen-polewidth/2,-polewidth/2
-        pole.v = [(l,b), (l,t), (r,t), (r,b)]
-
-        x = self.state
-        cartx = x[0]*scale+screen_width/2.0 # MIDDLE OF CART
-        self.carttrans.set_translation(cartx, carty)
-        self.poletrans.set_rotation(-x[2])
-
-        return self.viewer.render(return_rgb_array = mode=='rgb_array')
-
-    def close(self):
-        if self.viewer:
-            self.viewer.close()
-            self.viewer = None
+        :param mode:
+        :return:
+        """
+        return
