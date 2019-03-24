a9bb7a48b9ee1b604ebd910f72fda6de5548885c
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2019-03-24 15:18:48 -0400

    Fixed issue when training on cuda

--- shallow_Q_learner.py
+++ shallow_Q_learner.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-import csv 
-import gym 
+import csv
+import gym
 import gym_advection
-import random 
-import torch 
-from torch.autograd import Variable 
-import numpy as np 
-from utils.decay_schedule import LinearDecaySchedule 
+import random
+import torch
+from torch.autograd import Variable
+import numpy as np
+from utils.decay_schedule import LinearDecaySchedule
 from function_approximator.perceptron import SLP
 import os
 import progressbar
@@ -15,6 +15,7 @@ import sys
 env = gym.make("Advection-AdG-v0")
 MAX_NUM_EPISODES = int(sys.argv[1])
 MAX_STEPS_PER_EPISODE = 1200
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 class Shallow_Q_Learner(object):
 	def __init__(self, state_shape, action_shape, neurons=10, learning_rate=0.005, gamma=0.98):
 		self.state_shape = state_shape
@@ -23,7 +24,7 @@ class Shallow_Q_Learner(object):
 		self.learning_rate = learning_rate # Agent's Q-learning rate
 		# self.Q is the Action-Value function. This agent represents Q using a Neural Network.
 		self.neurons=int(neurons)
-		self.Q = SLP(state_shape, action_shape, self.neurons, device=torch.device("cpu"))
+		self.Q = SLP(state_shape, action_shape, self.neurons, device=torch.device(device)).to(device)
 		self.Q_optimizer = torch.optim.Adam(self.Q.parameters(), lr=1e-3)
 		# self.policy is the policy followed by the agent. This agents follows
 		# an epsilon-greedy policy w.r.t it's Q estimate.
@@ -32,32 +33,32 @@ class Shallow_Q_Learner(object):
 		self.epsilon_min = 0.05
 		self.epsilon_decay=LinearDecaySchedule(initial_value=self.epsilon_max, final_value=self.epsilon_min, max_steps= 0.5 * MAX_NUM_EPISODES	* MAX_STEPS_PER_EPISODE)
 		self.step_num = 0
-		
+
 	def get_action(self, observation):
 		return self.policy(observation)
-		
+
 	def epsilon_greedy_Q(self, observation):
 		# Decay Epsilion/exploratin as per schedule
 		#if random.random() < 0.05:
 		#print(self.epsilon_decay(self.step_num))
 		self.greedy_threshold=self.epsilon_decay(self.step_num)
 		if random.random() < self.greedy_threshold:
-			action = random.choice([i for i in range(self.action_shape)])	#Exploration		
+			action = random.choice([i for i in range(self.action_shape)])	#Exploration
 		else:
-			action = np.argmax(self.Q(observation).data.numpy())   #Exploitation
-		
+			action = np.argmax(self.Q(observation).data.to(torch.device('cpu')).numpy())   #Exploitation
+
 		#action = np.argmax(self.Q(observation).data.numpy())
 		return action
-		
+
 	def learn(self, s, a, r, s_next):
 		td_target = r + self.gamma * torch.max(self.Q(s_next))
-		td_error = torch.nn.functional.mse_loss(self.Q(s)[a], td_target)		
+		td_error = torch.nn.functional.mse_loss(self.Q(s)[a], td_target)
 		# Update Q estimate
 		#self.Q(s)[a] = self.Q(s)[a] + self.learning_rate * td_error
 		self.Q_optimizer.zero_grad()
 		td_error.backward()
-		self.Q_optimizer.step()		
-		
+		self.Q_optimizer.step()
+
 if __name__ == "__main__":
 	observation_shape = env.observation_space.shape
 	action_shape = env.action_space.n
@@ -80,7 +81,7 @@ if __name__ == "__main__":
 		episode_file=open(path+str(episode)+".csv", 'w')
 		episode_writer=csv.writer(episode_file, delimiter=',')
 		episode_writer.writerow(['dt', 'error'])
-		for step in range(MAX_STEPS_PER_EPISODE):		
+		for step in range(MAX_STEPS_PER_EPISODE):
 			# env.render()
 			agent.step_num+=1
 			action = agent.get_action(obs)
@@ -93,8 +94,8 @@ if __name__ == "__main__":
 			obs = next_obs
 			#if next_obs[0]>0.11 or done:
 			if env.tc/env.tmax>=1.0 or done:
-				break			
-			
+				break
+
 			'''
 			if done is True:
 				if first_episode: # Initialize max_reward at the end of first episode
@@ -105,13 +106,13 @@ if __name__ == "__main__":
 					max_reward = cum_reward
 				print("\nEpisode#{} ended in {} steps. reward ={} ;	mean_reward={} best_reward={}".format(episode, step+1, cum_reward,np.mean(episode_rewards), max_reward))
 				break
-			'''	
+			'''
 		episode_rewards.append(cum_reward)
 		print("Episode={0}, reward={1}, normalized time={2}, steps={3}, Average Reward={4}, Exploration threshold={5}".format(episode+1, cum_reward, env.tc/env.tmax, step, np.mean(episode_rewards), agent.greedy_threshold))
 		agent_writer.writerow([episode, cum_reward, env.tc/env.tmax, step, np.mean(episode_rewards)])
 		bar.update(episode+1)
 
 	bar.finish()
-	env.close()			
-				
-				
+	env.close()
+
+
